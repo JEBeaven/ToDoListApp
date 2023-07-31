7f05c49251e83e436f71c072beb2ba883ed2a24b
JEBeaven <joshuaeuan@hotmail.co.uk>
2023-07-31 20:53:19 +0100

    test

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("different add this line");
+        System.out.println("Test");
     }
 }

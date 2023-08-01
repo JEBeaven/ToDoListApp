b7fd1a50e001bf6c57c7c5b5c580f9fcdd6e1c00
JEBeaven <joshuaeuan@hotmail.co.uk>
2023-08-01 19:13:52 +0100

    Added the Priority to the tasks, next is to allow us to sort the tasks through their priority. However need to figure out the most efficient way of doing so, might implement a linked list.

--- src/App.java
+++ src/App.java
@@ -3,8 +3,8 @@ import java.time.LocalDate;
 public class App {
     public static void main(String[] args) throws Exception {
         ToDoList todoList = new ToDoList();
-        Task task1 = new Task("Complete assignment", "Finish the current group project on java", LocalDate.of(2023, 7, 31));
-        Task task2 = new Task("Study for exam", "Finish work on the origins of the world", LocalDate.of(2023, 8, 10));
+        Task task1 = new Task("Complete assignment", "Finish the current group project on java", LocalDate.of(2023, 7, 31), 2);
+        Task task2 = new Task("Study for exam", "Finish work on the origins of the world", LocalDate.of(2023, 8, 10), 5);
         todoList.addTask(task1);
         todoList.addTask(task2);
         todoList.showToDoList();
--- src/Task.java
+++ src/Task.java
@@ -3,26 +3,38 @@ import java.time.LocalDate;
 /**
  * The task class is used to represent a single task contained within the ToDo List
  * Within the task are attributes for the task which include:
- * name, description, dueDate, isCompleted
+ * name, description, dueDate, isCompleted, tasks priority
  */
 public class Task {
     private String name;
     private String description;
     private LocalDate dueDate;
     private boolean isCompleted;
+    private Integer priority;
 
     /**
      * /*
-     * The task Constructor that contrusts a task object with the given name, descrpition an its due date
+     * The task Constructor that contrusts a task object with the given name, descrpition, its due date, and the priority of the task
      * @param name        The name of the given task
      * @param description The descirption of the given task
      * @param dueDate     The due date of the given task
+     * @param priority    The tasks priority - 1:the lowest, 2: middle, 3: highest priority-must be completed before the 1 and 2.
      */
-    public Task(String name, String description, LocalDate dueDate){
+    public Task(String name, String description, LocalDate dueDate, Integer priority){
         this.name = name;
         this.description = description;
         this.dueDate = dueDate;
         this.isCompleted = false; //Tasks are automatically not completed.
+        if(priority < 1){
+            this.priority = 1;
+            System.out.println("Priority entered was less than 1, assumed you have meant 1");
+        }
+        if(priority > 3){
+            this.priority = 3;
+            System.out.println("Priority entered was greater than 3, assumed you have meant 3");
+        }
+        this.priority = priority;
+
     }
     /**
      * Return for the tasks name
@@ -40,9 +52,16 @@ public class Task {
     }
     /**
      * Return the Due date of the task
-     * @return
+     * @return dueDate
      */
     public LocalDate getDueDate(){
         return dueDate;
+    }
+    /**
+     * Return the priority of the task
+     * @return priority
+     */
+    public Integer getPriority(){
+        return priority;
     }
 }

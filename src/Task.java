0d81b58838d0003718cc8341c2e98e950b9d057e
JEBeaven <joshuaeuan@hotmail.co.uk>
2023-08-01 14:20:32 +0100

    Adding the task file

---
+++ src/Task.java
@@ -0,0 +1,26 @@
+import java.time.LocalDate;
+
+/**
+ * The task class is used to represent a single task contained within the ToDo List
+ * Within the task are attributes for the task which include:
+ * name, description, dueDate, isCompleted
+ */
+public class Task {
+    private String name;
+    private String description;
+    private LocalDate dueDate;
+    private boolean isCompleted;
+
+    /*
+     * The task Constructor that contrusts a task object with the given name, descrpition an its due date
+     * @param name        The name of the given task
+     * @param description The descirption of the given task
+     * @param dueDate     The due date of the given task
+     */
+    public Task(String name, String description, LocalDate dueDate){
+        this.name = name;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.isCompleted = false; //Tasks are automatically not completed.
+    }
+}
